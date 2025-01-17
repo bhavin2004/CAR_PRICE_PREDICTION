e030b6c751d2aba3591c49462e00710e21dcd0f2
Bhavin Karangia <omkarpro6@gmail.com>
2025-01-17 14:05:15 +0530

    add readme.md

--- app.py
+++ app.py
@@ -28,8 +28,7 @@ def train():
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
     companies = sorted(car['company'].unique())
-    company = request.form('company')
-    car_models = sorted(car[car['company']==company]['name'].unique())
+    car_models = sorted(car['name'].unique())
     years = sorted(car['year'].unique(), reverse=True)
     fuel_types = car['fuel_type'].unique()
 
